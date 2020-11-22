afc5616cae146808f52de07c0086c09bc9c1d9bb
Lee-kangchan <lsc4237@gmail.com>
2020-11-22 21:27:42 +0900

    MYSQL DATABASE변경

--- conf/mysql.js
+++ conf/mysql.js
@@ -8,7 +8,7 @@ var connection = mysql.createPool({
         port : 3306,
         user : 'root',
         password : '1111',
-        database:'allergy'
+        database:'allergy3'
 });
  
 // connection.connect(function(err) {
